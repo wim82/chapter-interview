0340d71cd9a986d7ca179169e819acf0dafc8a59
Wim Maesschalck <wimmaesschalck@gmail.com>
2018-05-02 22:24:57 +0200

    adds basic navigation

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import React from "react";
+import { Text, View, Button, YellowBox, StyleSheet } from "react-native";
 import QuestionRotator from "./QuestionRotator";
+import { StackNavigator } from "react-navigation";
 
-export default class App extends React.Component {
+YellowBox.ignoreWarnings(["Warning: isMounted(...) is deprecated"]);
+
+class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  static navigationOptions = ({ navigation }) => ({
+    title: "Interview",
+    headerRight: (
+      <Button
+        navigation={navigation}
+        onPress={() => navigation.navigate("Profile")}
+        title="Who"
+        color="red"
+      />
+    )
+  });
+
   render() {
-    return <QuestionRotator />;
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <QuestionRotator />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default StackNavigator(
+  {
+    Home: { screen: App },
+    Profile: { screen: App }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
