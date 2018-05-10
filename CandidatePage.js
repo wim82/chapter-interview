c186f90f80c361bcb9f4280c5a333a16d212d610
Wim Maesschalck <wimmaesschalck@gmail.com>
2018-05-10 23:55:48 +0200

    Adds date & position to the candidate form

--- CandidatePage.js
+++ CandidatePage.js
@@ -1,35 +1,80 @@
 import React from "react";
 
-import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  LayoutAnimation,
+  TouchableHighlight,
+  DatePickerIOS
+} from "react-native";
 
 export default class CandidatePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "hoi"
+      name: "",
+      date: new Date(),
+      isDatePickerOpen: false,
+      position: ""
     };
   }
+
+  setDate = newDate => {
+    console.log("setting new date", newDate);
+    this.setState({ date: newDate });
+  };
+
+  toggleDatePicker = previousState => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    console.log("togglin", this.state.isDatePickerOpen);
+    this.setState({ isDatePickerOpen: !this.state.isDatePickerOpen });
+  };
 
   render() {
     return (
       <View style={styles.main}>
         <ScrollView contentContainerStyle={[styles.container]}>
-          <Text onPress={this.onPress} style={styles.question}>
-            name
-          </Text>
+          <Text style={styles.question}>name</Text>
           <TextInput
             placeholder="Enter your name"
             onChangeText={name => this.setState({ name })}
             value={this.state.text}
             style={styles.nameInputField}
           />
-          <Text onPress={this.onPress} style={styles.question}>
-            date
-          </Text>
+          <Text style={styles.question}>date</Text>
+          <TouchableHighlight
+            underlayColor="#ddd"
+            onPress={this.toggleDatePicker}
+          >
+            <Text style={styles.dateInputField}>
+              {`${this.state.date.getDate()}/${this.state.date.getMonth() +
+                1}/${this.state.date.getFullYear()}`}
+            </Text>
+          </TouchableHighlight>
+          <View
+            style={
+              this.state.isDatePickerOpen
+                ? styles.datePickerOpen
+                : styles.datePickerClosed
+            }
+          >
+            <DatePickerIOS
+              mode="date"
+              date={this.state.date}
+              onDateChange={this.setDate}
+            />
+          </View>
+
+          <View>
+            <Text style={styles.question}>position</Text>
+          </View>
           <TextInput
-            placeholder="Still needs a datepicker"
-            onChangeText={name => this.setState({ name })}
-            value={this.state.text}
+            placeholder="For which position"
+            onChangeText={position => this.setState({ position })}
+            value={this.state.position}
             style={styles.nameInputField}
           />
         </ScrollView>
@@ -40,7 +85,7 @@ export default class CandidatePage extends React.Component {
 
 const styles = StyleSheet.create({
   main: {
-    backgroundColor: "white",
+    backgroundColor: "#00aaFF",
     flex: 1,
     alignItems: "stretch",
     justifyContent: "space-between"
@@ -50,19 +95,38 @@ const styles = StyleSheet.create({
     padding: "5% 0% 0% 5%",
     color: "#424242",
     opacity: 0.9,
-    height: 12,
-    fontVariant: ["small-caps"]
+    height: 8,
+    fontVariant: ["small-caps"],
+    marginBottom: -5
   },
   container: {
     flexGrow: 1,
     backgroundColor: "white"
   },
   nameInputField: {
-    paddingBottom: 6,
+    paddingBottom: 8,
+    paddingTop: 5,
     marginRight: "5%",
     marginLeft: "5%",
-    height: 36,
+    height: 40,
     fontSize: 24,
     color: "#424242"
+  },
+  dateInputField: {
+    paddingBottom: 8,
+    paddingTop: 5,
+    marginRight: "5%",
+    marginLeft: "5%",
+    height: 40,
+    fontSize: 24,
+    color: "#424242"
+  },
+  datePickerClosed: {
+    height: 0,
+    overflow: "hidden"
+  },
+  datePickerOpen: {
+    overflow: "hidden",
+    flex: 0
   }
 });
